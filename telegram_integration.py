1d3f9cb965267a3f6d528dfb45db68d6775572fa
Shoaib Hoque <shoaibhoque@gmail.com>
2024-06-27 00:04:59 +0600

    update data collection and make token hidden

--- telegram_integration.py
+++ telegram_integration.py
@@ -1,7 +1,8 @@
 import telegram
+import os
 
 def send_telegram_update(actual_price, predicted_price):
-    bot = telegram.Bot(token='7318185424:AAHfIWCYPdDNWx-e_m0qxElxEdhS4opZM1s')
+    bot = telegram.Bot(token=os.environ.get('TOKEN'))
     chat_id = '-2171247293'
     message = f"Actual price: {actual_price:.2f}\nPredicted price close: {predicted_price[0][0]:.2f}"
     bot.send_message(chat_id=chat_id, text=message)
